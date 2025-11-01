eaad403f38ce207a3963b3b15f1abbbb8644ef8c
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-10-31 19:22:02 -0600

    Test if cleanData removes data with year 2016 or later

--- src/AI/data/__Test__/data.test.js
+++ src/AI/data/__Test__/data.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { getData } from "../data";
+import { cleanUpData, getData } from "../data";
 
 describe("AI data functions", () => {
   describe("getData", () => {
@@ -10,6 +10,21 @@ describe("AI data functions", () => {
     it("Returns the expected array length", () => {
       const result = getData();
       expect(result.length).toEqual(45716);
+    });
+  });
+
+  describe("cleanData", () => {
+    it("Returns an array", () => {
+      const data = getData();
+      const result = data;
+      expect(Array.isArray(result)).toBeTruthy();
+    });
+    it("Removes data with year 2016 or later", () => {
+      const data = getData();
+      const result = cleanUpData(data);
+      expect(result.some((item) => parseInt(item.year, 10) >= 2016)).toEqual(
+        false
+      );
     });
   });
 });
--- src/AI/data/data.js
+++ src/AI/data/data.js
@@ -3,4 +3,13 @@ import csv from "./meteorite-landings.csv";
 export const getData = () => {
   return csv;
 };
-export const cleanUpData = () => {};
+export const cleanUpData = (data) => {
+  const cleanData = data.filter((item) => {
+    const parsed = parseInt(item.year);
+    if (parsed >= 2016) {
+      return false;
+    }
+    return true;
+  });
+  return cleanData;
+};
