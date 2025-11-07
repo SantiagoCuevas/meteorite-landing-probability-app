72d35ea05a9cda4944633af4fce5e8e3c069d896
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-11-07 13:21:53 -0700

    create function buildModel

--- src/AI/aiModel.js
+++ src/AI/aiModel.js
@@ -1,4 +1,3 @@
-// src/utils/training.js
 import * as tf from "@tensorflow/tfjs";
 
 const normLatitude = (latTile) => latTile / 90;
@@ -15,4 +14,15 @@ export function makeTrainingTensors(tileArray) {
   const xs = tf.tensor2d(xsArr);
   const ys = tf.tensor2d(ysArr);
   return { xs, ys, count: rows.length };
+}
+
+export function buildModel() {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.dense({ units: 16, activation: "relu", inputShape: [2] })
+  );
+  model.add(tf.layers.dense({ units: 16, activation: "relu" }));
+  model.add(tf.layers.dense({ units: 1, activation: "sigmoid" }));
+  model.compile({ optimizer: tf.train.adam(0.01), loss: "meanSquaredError" });
+  return model;
 }
