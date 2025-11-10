f1e26e7f7f71ec54dffb462a30d6703e5b9e67f4
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-11-10 12:56:17 -0700

    Mount TensorFlow test component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import "./App.css";
+import { cleanUpData, getData } from "./AI/data/data";
+import { PieChart } from "./components/PieChart";
+import { HistogramChart } from "./components/HistogramChart";
+import { LineTrendChart } from "./components/LineTrendChart";
+import { ModelTestHarness } from "./components/ModelTestComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const data = getData();
+  const cleanData = useMemo(() => cleanUpData(data), [data]);
+
+  // const findEntry = (input) => {
+  //   return input.find((item) => item.id === "18816");
+  // };
+  // const findCleanEntry = (input) => {
+  //   return input.find((item) => item.id === 18816);
+
+  // const entry = findEntry(data);
+  // const findNewEntry = findCleanEntry(cleanData);
+
+  // console.log("xx uncleaned", entry);
+  // console.log("xx cleaned", findNewEntry);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ModelTestComponent />
+      <div style={{ width: "700px", height: "500px", margin: "0 auto" }}>
+        <PieChart cleanData={cleanData} />
       </div>
-      <h1>Vite + React</h1>
+      <div style={{ width: "700px", height: "500px", margin: "0 auto" }}>
+        <HistogramChart cleanData={cleanData} />
+      </div>
+      <div style={{ width: "700px", height: "500px", margin: "0 auto" }}>
+        <LineTrendChart cleanData={cleanData} />
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
